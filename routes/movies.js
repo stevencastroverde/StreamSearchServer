60b47e0bd6fd8ba71d04a013b8c1acb07413e527
Steven <sfcastroverde@iMac.PK5001Z>
2017-01-31 21:28:25 -0700

    fixed search movie route

--- routes/movies.js
+++ routes/movies.js
@@ -8,15 +8,15 @@ router.get('/', function(req, res) {
   Guidebox.getFreeMovies()
     .then((response) => res.json(response));
 });
+router.get('/search/:movieTitle',function(req,res){
+	Guidebox.searchMovies(req.params.movieTitle)
+		.then(response => res.json(response));
+});
 router.get('/:id', function(req, res){
   Guidebox.getSpecificMovie(req.params.id)
     .then((response) => res.json(response));
 });
 
-router.get('/:movieTitle',function(req,res){
-	Guidebox.searchMovies(req.params.movieTitle)
-		.then(response => res.json(response));
 
-})
 
 module.exports = router;
